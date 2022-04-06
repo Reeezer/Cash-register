b3668a3d95617fe54341eb819c22195c2e247746
Luca Meyer <luca.frostblue@gmail.com>
2022-04-06 13:28:01 +0200

    Added sample database connection code

---
+++ CashDatabase/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace CashDatabase
+{
+    public class CashDatabaseContext : DbContext
+    {
+        private readonly string connectionString;
+        
+        public CashDatabaseContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(connectionString);
+    }
+
+    public class Program
+    {
+    
+    }
+    
+}
