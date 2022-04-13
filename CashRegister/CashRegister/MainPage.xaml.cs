1760f9484aa5efbd4dd04820d5c1af58a5740204
Luca Campana <luca.campana@he-arc.ch>
2022-04-13 15:34:05 +0200

    Working scan with API OpenFoodFacts fetch

--- CashRegister/CashRegister/MainPage.xaml.cs
+++ CashRegister/CashRegister/MainPage.xaml.cs
@@ -7,6 +7,12 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using CashRegister.Services;
+using System.Diagnostics;
+using OpenFoodFacts4Net.ApiClient;
+using OpenFoodFacts4Net.Csv;
+using OpenFoodFacts4Net.Json.Data;
+using OpenFoodFacts4Net.Taxonomy.Json;
+using OpenFoodFacts4Net.Taxonomy.Json.Data;
 
 namespace CashRegister
 {
@@ -28,9 +34,12 @@ namespace CashRegister
             {
                 var scanner = DependencyService.Get<IQrScanningService>();
                 var result = await scanner.ScanAsync();
+
                 if (result != null)
                 {
                     txtBarcode.Text = result;
+                    GetProductAsync(txtBarcode.Text);
+
                 }
             }
             catch (Exception ex)
@@ -38,5 +47,24 @@ namespace CashRegister
                 throw;
             }
         }
+
+        private async Task GetProductAsync(string barcode)
+        {
+            try
+            {
+                String userAgent = UserAgentHelper.GetUserAgent("OpenFoodFacts4Net.ApiClient.CashRegister", ".Net Standard", "2.0", null);
+                Client client = new Client(Constants.BaseUrl, userAgent);
+                GetProductResponse productResponse = await client.GetProductAsync(barcode);
+                Console.WriteLine(productResponse.Product.ProductName);
+                txtArticleDescr.Text = productResponse.Product.ProductName;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Article not found");
+                txtArticleDescr.Text = "Article not found";
+            }
+
+        }
+
     }
 }
