e12f55e0ff2c88996650e5d277d885de10ea70c9
Luca Campana <luca.campana@he-arc.ch>
2022-05-04 14:24:43 +0200

    Commented the Models

--- CashRegister/CashRegister/Model/Discount.cs
+++ CashRegister/CashRegister/Model/Discount.cs
@@ -5,15 +5,34 @@ namespace CashRegister.Model
     public class Discount
     {
         /// <summary>
-        /// IMPORTANT : Do NOT change the ID manually
+        /// IMPORTANT : Do NOT change the ID manually. It is handled by the DB.
         /// </summary>
         public int Id { get; set; } = 0;
 
+        /// <summary>
+        /// Start date of the discount
+        /// </summary>
         public DateTime StartDate { get; set; }
+        /// <summary>
+        /// End date of the discount
+        /// </summary>
         public DateTime EndDate { get; set;  }
+        /// <summary>
+        /// Category of the discount concerned by the discount
+        /// </summary>
         public Category Category { get; set; }
+        /// <summary>
+        /// Percentage of the discount
+        /// </summary>
         public int Percentage { get; set; }
 
+        /// <summary>
+        /// Constructor with all parameters
+        /// </summary>
+        /// <param name="startDate">The start date of the discount</param>
+        /// <param name="endDate">The end date of the discount</param>
+        /// <param name="category">The category concerned of the discount</param>
+        /// <param name="percentage">The percentage of the discount</param>
         public Discount(DateTime startDate, DateTime endDate, Category category, int percentage)
         {
             StartDate = startDate;
@@ -22,6 +41,9 @@ namespace CashRegister.Model
             Percentage = percentage;
         }
 
+        /// <summary>
+        /// Empty constructor
+        /// </summary>
         public Discount()
         {
         }
--- CashRegister/CashRegister/Model/ReceiptLine.cs
+++ CashRegister/CashRegister/Model/ReceiptLine.cs
@@ -6,13 +6,22 @@ namespace CashRegister.Model
     public class ReceiptLine : ViewModelBase, IComparable
     {
         /// <summary>
-        /// IMPORTANT : Do NOT change the ID manually
+        /// IMPORTANT : Do NOT change the ID manually. It is handled by the DB.
         /// </summary>
         public int Id { get; set; } = 0;
 
+        /// <summary>
+        /// The receipt linked th the line
+        /// </summary>
         public Receipt Receipt { get; set; }
+        /// <summary>
+        /// The item of the line
+        /// </summary>
         public Item Item { get; set; }
 
+        /// <summary>
+        /// Quantity of items on the line
+        /// </summary>
         private int quantity;
         public int Quantity
         {
@@ -24,6 +33,9 @@ namespace CashRegister.Model
             }
         }
 
+        /// <summary>
+        /// The price of the items on the line
+        /// </summary>
         private double linePrice;
         public double LinePrice
         {
@@ -35,6 +47,12 @@ namespace CashRegister.Model
             }
         }
 
+        /// <summary>
+        /// Contructor of a ReceiptLine
+        /// </summary>
+        /// <param name="receipt">The Receipt linked to the Line</param>
+        /// <param name="item">The item present to  the line</param>
+        /// <param name="quantity">The quantity of items on the Line</param>
         public ReceiptLine(Receipt receipt, Item item, int quantity=1)
         {
             Receipt = receipt;
@@ -43,27 +61,46 @@ namespace CashRegister.Model
             LinePrice = Quantity * Item.Price;
         }
 
+        /// <summary>
+        /// Emnpty constructor
+        /// </summary>
         public ReceiptLine()
         {
         }
 
+        /// <summary>
+        /// To add an item on the Line
+        /// </summary>
+        /// <param name="quantity"></param>
         public void AddItem(int quantity=1)
         {
             Quantity += quantity;
             RefreshLinePrice();
         }
 
+        /// <summary>
+        /// To remoove an item of the line
+        /// </summary>
+        /// <param name="quantity"></param>
         public void RemoveItem(int quantity=1)
         {
             Quantity -= quantity;
             RefreshLinePrice();
         }
 
+        /// <summary>
+        /// To refresh the price of the line
+        /// </summary>
         private void RefreshLinePrice()
         {
             LinePrice = Quantity * Item.Price;
         }
 
+        /// <summary>
+        /// Helps to compare two Lines
+        /// </summary>
+        /// <param name="obj">Line to compare with</param>
+        /// <returns></returns>
         public int CompareTo(object obj)
         {
             ReceiptLine c2 = obj as ReceiptLine;
